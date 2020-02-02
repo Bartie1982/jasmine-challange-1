6c2cc2449529b6230b5073f9b20c8a7aacd82856
Bartie1982 <bartosz.s@hotmail.com>
2020-02-02 19:20:48 +0000

    classSpec.js base

---
+++ spec/calc.Spec.js
@@ -0,0 +1,33 @@
+  
+describe("What can I drink", function() {
+
+    beforeEach(function() {
+         
+  var age = number;
+
+            it("Cant tell", function() {
+            age = 0;
+            expect(age).toBe();
+        });
+            it("Drink Toddy", function() {
+            age <= 14;
+            expect(age).toBe();
+        });
+            it("Drink Coke", function() {
+            age <= 18;
+            expect(age).toBe();
+        });
+            it("Drink Beer", function() {
+            age <= 21;
+            expect(age).toBe();
+        });
+            it("Drink whiskey", function() {
+            age <= 130;
+            expect(age).toBe();
+        });
+
+
+    });
+});
+
+  
