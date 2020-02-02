605db0d10013915089fc121f02de029654a8b384
Bartie1982 <bartosz.s@hotmail.com>
2020-02-02 19:53:15 +0000

    dont understand spec.js need to use source code help

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,24 +1,21 @@
-whatCanIDrink = function() {
-    this.value = 0;
-};
+whatCanIDrink = function(age) {
 
-whatCanIDrink= function(number) {
-    if (number <= 0 ) {
-        return("Sorry. I can’t tell what drink because that age is incorrect!");
+    if (number < 0 ) {
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
     } 
-    if (number <= 14 ) {
-        return("Drink Toddy");
+    else if (number < 14 ) {
+        return "Drink Toddy";
     } 
-    if (number > 14 && number <= 18  ) {
-        return("Drink Coke");
+    else if (number >= 14 && number < 18  ) {
+        return "Drink Coke";
     } 
-    if (number > 18 && number <= 21 ) {
-        return("Drink Beer");
+    else if (number >= 18 && number < 21 ) {
+        return "Drink Beer";
     } 
-    if (number >18 && number <= 130 ) {
-        return("Drink Whiskey");
+    else if (number >= 21 && number < 130 ) {
+        return "Drink Whiskey";
     } else { 
-        return("Sorry. I can’t tell what drink because that age is incorrect!");
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
     }     
 };    
     
--- spec/calc.Spec.js
+++ spec/calc.Spec.js
@@ -2,32 +2,37 @@
 describe("What can I drink", function() {
 
     beforeEach(function() {
+        drink = new whatCanIDrink();
+    }); 
          
-  var age = number;
+    describe("Test age", function() {
+            it("should exist", function() {
+            expect(whatCanIDrink).toBeDefined();
+        });    
 
             it("Cant tell", function() {
             age = 0;
-            expect(age).toBe();
+            expect(age).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         });
             it("Drink Toddy", function() {
             age <= 14;
-            expect(age).toBe();
+            expect(age).toBe("Drink Toddy");
         });
             it("Drink Coke", function() {
             age <= 18;
-            expect(age).toBe();
+            expect(age).toBe("Drink Coke");
         });
             it("Drink Beer", function() {
             age <= 21;
-            expect(age).toBe();
+            expect(age).toBe("Drink Beer");
         });
             it("Drink whiskey", function() {
             age <= 130;
-            expect(age).toBe();
+            expect(age).toBe("Drink Whisky");
         });
 
 
     });
 });
 
-  
+ 
